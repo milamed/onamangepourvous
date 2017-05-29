b2323dfc64c8e6ef4cb712e193cd8f6fbd53e635
Yessine Abid <yessine.abid@live.fr>
2017-05-29 22:18:00 +0100

    change designe

--- app/src/main/java/com/ompv/amigos/myapplication/InterfacePublication.java
+++ app/src/main/java/com/ompv/amigos/myapplication/InterfacePublication.java
@@ -42,6 +42,7 @@ public class InterfacePublication extends AppCompatActivity implements View.OnCl
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interface_publication);
+        getSupportActionBar().hide();
         initViews();
 
         filename = getIntent().getStringExtra("pic");
